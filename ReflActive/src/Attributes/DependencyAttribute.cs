7ac4cc0fc8367518dbeb1d38689384615851dc15
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2025-08-03 22:02:41 -0400

    fix typo

--- ReflActive/src/Attributes/DependencyAttribute.cs
+++ ReflActive/src/Attributes/DependencyAttribute.cs
@@ -4,7 +4,7 @@ namespace ReflActive.Attributes;
 
 /// <summary>
 /// The <c>DependencyAttribute</c> annotates parameters to a constructor that exhibits the <see cref="ActivationTargetConstructorAttribute"/>
-/// the may be bound to any member of a <see cref="Type"/> for which a <see cref="IActivationContext.GetDependency">
+/// that may be bound to any member of a <see cref="Type"/> for which a <see cref="IActivationContext.GetDependency">
 /// dependency</see> exists.
 /// </summary>
 [AttributeUsage(AttributeTargets.Parameter)]
